a42f8e3eb3681bc2be92c7def86159ef38fceda7
Domenico Di Ruocco <domenico@Domenicos-MacBook-Pro.local>
2020-11-28 20:07:10 +0100

    API support

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import slack
 import os
 import json
 import requests
+import concurrent.futures
+from urllib.parse import unquote
 from slackeventsapi import SlackEventAdapter
 from threading import Thread
 from pathlib import Path
@@ -40,89 +42,155 @@ def check_reaction(payload):
 #slack slash commands
 
 #test command to check if the bot is installed
-@app.route("/test", methods=["POST"])
+@app.route("/test", methods=["POST", "GET"])
 def test():
-    data = request.form
-    cid = data.get("channel_id")
-    cname = data.get("channel_name")
-    client.chat_postMessage(channel=cid, text=f"This app is working properly in #{cname}", icon_emoji=":newspaper:")
-    return Response(), 200
+    if request.method == "POST":
+        data = request.form
+        cid = data.get("channel_id")
+        cname = data.get("channel_name")
+        client.chat_postMessage(channel=cid, text=f"This app is working properly in #{cname}", icon_emoji=":newspaper:")
+        return Response(), 200
+    else:
+        return json.dumps({"message":"visit /help to get started"})
 
 
 #help command
-@app.route("/help", methods=["POST"])
+@app.route("/help", methods=["POST", "GET"])
 def help():
-    data = request.form
-    cid = data.get("channel_id")
-    txt = data.get("text")
-    response_url = data.get("response_url")
-    help_msg = HelpMessage(cid, response_url, client, txt)
-    thr = Thread(target=help_msg.go)
-    try:
-        thr.start()
-    except:
-        return requests.post(response_url,data=json.dumps({"text":"Fatal Error","username": "slack-news", "icon_emoji":":newspaper:"})) 
-    return Response(), 200
-
-#search news by keyword
-@app.route("/search-news", methods=["POST"])
-def keywordSearch():
-    data = request.form
-    cid = data.get("channel_id")
-    txt = data.get("text")
-    response_url = data.get("response_url")
-    if txt.strip() != "":
-        payload = {"text":"please wait...","username": "slack-news", "icon_emoji":":newspaper:"}
-        requests.post(response_url,data=json.dumps(payload))
-        news = ByKeyword(txt, cid, response_url, client)
-        thr = Thread(target=news.go)
+    if request.method == "POST":
+        data = request.form
+        cid = data.get("channel_id")
+        txt = data.get("text")
+        response_url = data.get("response_url")
+        help_msg = HelpMessage(cid, response_url, client, txt)
+        thr = Thread(target=help_msg.go)
         try:
             thr.start()
         except:
             return requests.post(response_url,data=json.dumps({"text":"Fatal Error","username": "slack-news", "icon_emoji":":newspaper:"})) 
+        return Response(), 200
     else:
-        requests.post(response_url,data=json.dumps({"text":"search for a keyword","username": "slack-news", "icon_emoji":":newspaper:"})) 
-    return Response(), 200
+        _help = HelpMessage(None, None, None, request.args.get("command"))
+        raw = {}
+        try:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future = executor.submit(_help.web)
+                raw = future.result()
+        except:
+            return Response(), 500
+        return Response(json.dumps(raw)), 200
+
+#search news by keyword
+@app.route("/search-news", methods=["POST", "GET"])
+def keywordSearch():
+    if request.method == "POST":
+        data = request.form
+        cid = data.get("channel_id")
+        txt = data.get("text")
+        response_url = data.get("response_url")
+        if txt.strip() != "":
+            payload = {"text":"please wait...","username": "slack-news", "icon_emoji":":newspaper:"}
+            requests.post(response_url,data=json.dumps(payload))
+            news = ByKeyword(txt, cid, response_url, client)
+            thr = Thread(target=news.go)
+            try:
+                thr.start()
+            except:
+                return requests.post(response_url,data=json.dumps({"text":"Fatal Error","username": "slack-news", "icon_emoji":":newspaper:"})) 
+        else:
+            requests.post(response_url,data=json.dumps({"text":"search for a keyword","username": "slack-news", "icon_emoji":":newspaper:"})) 
+        return Response(), 200
+    else:
+        if request.args.get("keyword"):
+            keyword = unquote(request.args.get("keyword"))
+            news = ByKeyword(keyword, None, None, None)
+            raw = {}
+            try:
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    future = executor.submit(news.web)
+                    raw = future.result()
+            except:
+                return Response(), 500
+            if raw:
+                data = raw.get("raw_res")
+                return Response(json.dumps(data)), 200
+            else:
+                return Response(json.dumps({"Error":"No Results"})), 404
+        else:
+            return Response(json.dumps({"Error":"No keyword"})), 404
 
 
 #random news
-@app.route("/random-news", methods=["POST"])
+@app.route("/random-news", methods=["POST", "GET"])
 def random_news():
-    data = request.form
-    cid = data.get("channel_id")
-    response_url = data.get("response_url")
-    payload = {"text":"please wait...","username": "slack-news", "icon_emoji":":newspaper:"}
-    requests.post(response_url,data=json.dumps(payload))
-    news = RandomNews(cid, response_url, client)
-    thr = Thread(target=news.go)
-    try:
-        thr.start()
-    except:
-        return requests.post(response_url,data=json.dumps({"text":"Fatal Error","username": "slack-news", "icon_emoji":":newspaper:"})) 
-    return Response(), 200
-
-
-#searches for news by url
-@app.route("/search-url", methods=["POST"])
-def urlSearch():
-    data = request.form
-    cid = data.get("channel_id")
-    input_url = data.get("text")
-    response_url = data.get("response_url")
-    if input_url.strip() != "":
+    if request.method == "POST":
+        data = request.form
+        cid = data.get("channel_id")
+        response_url = data.get("response_url")
         payload = {"text":"please wait...","username": "slack-news", "icon_emoji":":newspaper:"}
         requests.post(response_url,data=json.dumps(payload))
-        news = ByURL(input_url, cid, response_url, client)
+        news = RandomNews(cid, response_url, client)
         thr = Thread(target=news.go)
         try:
             thr.start()
         except:
             return requests.post(response_url,data=json.dumps({"text":"Fatal Error","username": "slack-news", "icon_emoji":":newspaper:"})) 
+        return Response(), 200
     else:
-        requests.post(response_url,data=json.dumps({"text":"please search for an url","username": "slack-news", "icon_emoji":":newspaper:"})) 
-    
-    return Response(), 200
+        news = RandomNews(None, None, None)
+        #thr = Thread(target=news.web)
+        raw = {}
+        try:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future = executor.submit(news.web)
+                raw = future.result()
+        except:
+            return Response(), 500
+        if raw:
+            data = raw.get("raw_res")
+            return Response(json.dumps(data)), 200
+        else:
+            return Response(json.dumps({"Error":"No Results"})), 404
 
+
+#searches for news by url
+@app.route("/search-url", methods=["POST", "GET"])
+def urlSearch():
+    if request.method == "POST":
+        data = request.form
+        cid = data.get("channel_id")
+        input_url = data.get("text")
+        response_url = data.get("response_url")
+        if input_url.strip() != "":
+            payload = {"text":"please wait...","username": "slack-news", "icon_emoji":":newspaper:"}
+            requests.post(response_url,data=json.dumps(payload))
+            news = ByURL(input_url, cid, response_url, client)
+            thr = Thread(target=news.go)
+            try:
+                thr.start()
+            except:
+                return requests.post(response_url,data=json.dumps({"text":"Fatal Error","username": "slack-news", "icon_emoji":":newspaper:"})) 
+        else:
+            requests.post(response_url,data=json.dumps({"text":"please search for an url","username": "slack-news", "icon_emoji":":newspaper:"})) 
+        return Response(), 200
+    else:
+        if request.args.get("url"):
+            _url = unquote(request.args.get("url"))
+            news = ByURL(_url, None, None, None)
+            raw = {}
+            try:
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    future = executor.submit(news.web)
+                    raw = future.result()
+            except:
+                return Response(), 500
+            if raw:
+                data = raw.get("raw_res")
+                return Response(json.dumps(data)), 200
+            else:
+                return Response(json.dumps({"Error":"No Results"})), 404
+        else:
+            return Response(json.dumps({"Error":"No URL"})), 404
 
 #deletes all the messages sent from the bot, to be deleted after testing
 def nukeAction(url, cid, msgs): 
