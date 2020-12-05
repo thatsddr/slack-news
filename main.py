ab6e2cc3de430c94b2ae5d4cd69dfafae1b3a6fb
Domenico Di Ruocco <domenico@Domenicos-MacBook-Pro.local>
2020-12-05 10:39:43 +0100

    comments added

--- main.py
+++ main.py
@@ -25,22 +25,27 @@ slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/eve
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")["user_id"]
 
-#new messages handled here
+#check if the bot is mentioned in a thread every time a message is posted
 @slack_event_adapter.on('message')
 def message(payload):
+    #get basic information
     event = payload.get('event', {})
     text = event.get("text", {})
     cid = event.get("channel")
     thread_ts = event.get("thread_ts", None)
+    #function to check if a string contains a URL
     def check_url(string):    
         regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
         url = re.findall(regex,string)       
         return [x[0] for x in url]
+    #check if the bot is mentioned in a thread
     if BOT_ID in text and thread_ts:
+        #if yes, get the original message and try to get URL(s) out of it
         res = client.conversations_history(channel=cid, latest=thread_ts, inclusive=True,limit=1)
         message_str = res.get('messages', [{}])[0].get("text", None)
         raw_urls = (check_url(message_str))
         urls = []
+        #if there are URLSs, clean the link from the markdown stuff
         if raw_urls:
             for url in raw_urls:
                 index = None
@@ -55,6 +60,7 @@ def message(payload):
                 else:
                     if url not in urls:
                         urls.append(url)
+            #for each of the clean URLs, search news by URL
             for url in urls:
                 news = ByURL(url, cid, None, client, thread=thread_ts)
                 thr = Thread(target=news.go_thread)
@@ -69,9 +75,10 @@ def message(payload):
 #reaction handler that deletes a message if it was sent from the bot and the user reacts with :x:
 @slack_event_adapter.on('reaction_added')
 def check_reaction(payload):
+    #get basic info
     event = payload.get('event', {})
     channel_id = event.get("item", {}).get("channel")
-
+    #if the reaction is :x:, delete the message
     if payload["event"]["reaction"] == "x":
         try:
             client.chat_delete(
@@ -80,15 +87,20 @@ def check_reaction(payload):
         except:
             pass
 
+
 #slack slash commands
+
 
 #test command to check if the bot is installed
 @app.route("/test", methods=["POST", "GET"])
 def test():
+    #Send slack response if the method is POST or return some json if the method is GET
     if request.method == "POST":
+        #get basic info
         data = request.form
         cid = data.get("channel_id")
         cname = data.get("channel_name")
+        #send message
         client.chat_postMessage(channel=cid, text=f"This app is working properly in #{cname}")
         return Response(), 200
     else:
@@ -98,22 +110,28 @@ def test():
 #help command
 @app.route("/help", methods=["POST", "GET"])
 def help():
+    #Send slack response if the method is POST or return some json if the method is GET
     if request.method == "POST":
+        #get basic info
         data = request.form
         cid = data.get("channel_id")
         txt = data.get("text")
         response_url = data.get("response_url")
+        #initialize class
         help_msg = HelpMessage(cid, response_url, client, txt)
         thr = Thread(target=help_msg.go)
         try:
+            #try to send the message
             thr.start()
         except:
             return requests.post(response_url,data=json.dumps({"text":"Fatal Error"})) 
         return Response(), 200
     else:
+        #initialize the class
         _help = HelpMessage(None, None, None, request.args.get("command"))
         raw = {}
         try:
+            #wait for the code to get some data and then return it
             with concurrent.futures.ThreadPoolExecutor() as executor:
                 future = executor.submit(_help.web)
                 raw = future.result()
@@ -124,16 +142,22 @@ def help():
 #search news by keyword
 @app.route("/search-news", methods=["POST", "GET"])
 def keywordSearch():
+    #Send slack response if the method is POST or return some json if the method is GET
     if request.method == "POST":
+        #get basic data
         data = request.form
         cid = data.get("channel_id")
         txt = data.get("text")
         response_url = data.get("response_url")
+        #if there is a keyword, search for it
         if txt.strip() != "":
+            #tell the user he'll gwt his news
             payload = {"text":"please wait..."}
             requests.post(response_url,data=json.dumps(payload))
+            #initialize the class
             news = ByKeyword(txt, cid, response_url, client)
             thr = Thread(target=news.go)
+            #try to execute the code, if it fails return an error
             try:
                 thr.start()
             except:
@@ -142,16 +166,20 @@ def keywordSearch():
             requests.post(response_url,data=json.dumps({"text":"search for a keyword"})) 
         return Response(), 200
     else:
+        #in order to return some json, check if there is a keyword
         if request.args.get("keyword"):
             keyword = unquote(request.args.get("keyword"))
+            #initialize the class
             news = ByKeyword(keyword, None, None, None)
             raw = {}
+            #try to get a response...
             try:
                 with concurrent.futures.ThreadPoolExecutor() as executor:
                     future = executor.submit(news.web)
                     raw = future.result()
             except:
                 return Response(), 500
+            #...and return only the part of data not formatted for slack
             if raw:
                 data = raw.get("raw_res")
                 return Response(json.dumps(data)), 200
@@ -164,29 +192,36 @@ def keywordSearch():
 #random news
 @app.route("/random-news", methods=["POST", "GET"])
 def random_news():
+    #Send slack response if the method is POST or return some json if the method is GET
     if request.method == "POST":
+        #get basic info
         data = request.form
         cid = data.get("channel_id")
         response_url = data.get("response_url")
+        #tell the user he'll get his response
         payload = {"text":"please wait (this operation can take a while)..."}
         requests.post(response_url,data=json.dumps(payload))
+        #initialize the class
         news = RandomNews(cid, response_url, client)
         thr = Thread(target=news.go)
+        #try to respond
         try:
             thr.start()
         except:
             return requests.post(response_url,data=json.dumps({"text":"Fatal Error"})) 
         return Response(), 200
     else:
+        #initialize the class for the API
         news = RandomNews(None, None, None)
-        #thr = Thread(target=news.web)
         raw = {}
+        #try to get data
         try:
             with concurrent.futures.ThreadPoolExecutor() as executor:
                 future = executor.submit(news.web)
                 raw = future.result()
         except:
             return Response(json.dumps({"Error":"An unexpected internal error occured"})), 500
+        #return only the non markdown data
         if raw:
             data = raw.get("raw_res")
             return Response(json.dumps(data)), 200
@@ -197,16 +232,22 @@ def random_news():
 #searches for news by url
 @app.route("/search-url", methods=["POST", "GET"])
 def urlSearch():
+    #Send slack response if the method is POST or return some json if the method is GET
     if request.method == "POST":
+        #get basic info
         data = request.form
         cid = data.get("channel_id")
         input_url = data.get("text")
         response_url = data.get("response_url")
+        #i f the url is specifies proceed
         if input_url.strip() != "":
+            #tell the suer he'll get his response
             payload = {"text":"please wait..."}
             requests.post(response_url,data=json.dumps(payload))
+            #initialize the class
             news = ByURL(input_url, cid, response_url, client)
             thr = Thread(target=news.go)
+            #try to respond
             try:
                 thr.start()
             except:
@@ -215,16 +256,20 @@ def urlSearch():
             requests.post(response_url,data=json.dumps({"text":"please search for an url"})) 
         return Response(), 200
     else:
+        #in order to return some json, check if there is a specified URL
         if request.args.get("url"):
             _url = unquote(request.args.get("url"))
+            #initialize the class
             news = ByURL(_url, None, None, None)
             raw = {}
+            #try to get some data
             try:
                 with concurrent.futures.ThreadPoolExecutor() as executor:
                     future = executor.submit(news.web)
                     raw = future.result()
             except:
                 return Response(), 500
+            #if there is data only return the non formatted data
             if raw:
                 data = raw.get("raw_res")
                 return Response(json.dumps(data)), 200
